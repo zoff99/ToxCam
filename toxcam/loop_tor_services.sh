1e7e9526f7dafb9b299d351d9491564b018d161d
Zoff <zoff99@users.noreply.github.com>
2017-09-01 17:36:58 +0200

    add tor param

--- toxcam/loop_tor_services.sh
+++ toxcam/loop_tor_services.sh
@@ -1,1 +1,16 @@
 
+#! /bin/bash
+
+#####################################################
+# pick first available video device
+# change for your needs here!
+video_device=$(ls -1 /dev/video*|tail -1)
+#
+#####################################################
+
+cd $(dirname "$0")
+
+while [ 1 == 1 ]; do
+	./toxcam_static -d $video_device -2 -T # > /dev/null 2> /dev/null
+	sleep 10
+done
