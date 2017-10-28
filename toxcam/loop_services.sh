de74abe939206988cba75a49c037a4e829cc2b29
Zoff <zoff99@users.noreply.github.com>
2017-10-28 10:39:57 +0200

    Update loop_services.sh

--- toxcam/loop_services.sh
+++ toxcam/loop_services.sh
@@ -10,7 +10,9 @@ video_device=$(ls -1 /dev/video*|tail -1)
 cd $(dirname "$0")
 
 while [ 1 == 1 ]; do
-	./toxcam_static -d $video_device -2 # > /dev/null 2> /dev/null
+        v4l2-ctl -d "$video_device" -v width=1280,height=720,pixelformat=YV12
+        v4l2-ctl -d "$video_device" -p 25
+	./toxcam_static -f -d $video_device -2 # > /dev/null 2> /dev/null
 	sleep 10
 done
 
