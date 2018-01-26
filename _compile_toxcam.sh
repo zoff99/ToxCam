6396c033915e41006d8076c067aa91b30dc279f5
zoff99 <zoff@zoff.cc>
2018-01-26 23:15:53 +0100

    compile

--- _compile_toxcam.sh
+++ _compile_toxcam.sh
@@ -19,6 +19,9 @@ echo $_INST_
 
 cd $_HOME_/toxcam
 
+
+if [ 1 == 1 ]; then
+
 gcc -O3 -g -Wall \
 -o toxcam_static \
 toxcam.c \
@@ -30,3 +33,19 @@ rb.c \
 -lsodium -lpthread -static-libgcc -static-libstdc++ -lopus \
 -lvpx -lm -lpthread -lv4lconvert -ljpeg -lm -lrt -lpthread
  
+else
+
+gcc \
+-fsanitize=address \
+-O3 -g -Wall \
+-o toxcam \
+toxcam.c \
+rb.c \
+-std=gnu99 \
+-L$_INST_/lib -I$_INST_/include/ \
+-lsodium \
+-ltoxcore -ltoxav \
+-lsodium -lpthread -static-libgcc -static-libstdc++ -lopus \
+-lvpx -lm -lpthread -lv4lconvert -ljpeg -lm -lrt -lpthread
+
+fi
