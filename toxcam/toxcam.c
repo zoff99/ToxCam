76fbc4090bc32d841970fdecfd3e55a33e1831d5
Zoff <zoff99@users.noreply.github.com>
2018-02-02 17:16:33 +0100

    tweak

--- toxcam/toxcam.c
+++ toxcam/toxcam.c
@@ -32,7 +32,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include <time.h>
+
+#ifndef _IS_PLATFORM_WIN_
+ #include <sys/time.h>
+#else
+ #include <time.h>
+#endif
+
 #include <dirent.h>
 #include <math.h>
 
@@ -59,6 +65,44 @@
 #include <vpx/vpx_image.h>
 #ifndef _IS_PLATFORM_WIN_
  #include <sys/mman.h>
+#endif
+
+
+
+
+
+#ifdef _IS_PLATFORM_WIN_
+
+/*
+   Implementation as per:
+   The Open Group Base Specifications, Issue 6
+   IEEE Std 1003.1, 2004 Edition
+
+   The timezone pointer arg is ignored.  Errors are ignored.
+*/
+
+#ifdef	__cplusplus
+
+void  GetSystemTimeAsFileTime(FILETIME*);
+
+inline int gettimeofday(struct timeval* p, void* tz /* IGNORED */)
+{
+	union {
+	    long long ns100; /*time since 1 Jan 1601 in 100ns units */
+		FILETIME ft;
+	} now;
+
+    GetSystemTimeAsFileTime( &(now.ft) );
+    p->tv_usec=(long)((now.ns100 / 10LL) % 1000000LL );
+    p->tv_sec= (long)((now.ns100-(116444736000000000LL))/10000000LL);
+	return 0;
+}
+
+#else
+    /* Must be defined somewhere else */
+	int gettimeofday(struct timeval* p, void* tz /* IGNORED */);
+#endif
+
 #endif
 
 
@@ -4140,6 +4184,14 @@ void sigint_handler(int signo)
 
 int main(int argc, char *argv[])
 {
+
+
+	printf("AAAAA:001\n");
+	exit(0);
+
+
+
+
     global_want_restart = 0;
     global_video_active = 0;
     global_send_first_frame = 0;
