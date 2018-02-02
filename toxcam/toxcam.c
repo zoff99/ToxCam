bc3c9ff192816965b16a33cc0abd5bb27d1288eb
zoff99 <zoff@zoff.cc>
2018-02-02 21:59:00 +0100

    wincompile

--- toxcam/toxcam.c
+++ toxcam/toxcam.c
@@ -25,6 +25,7 @@
 
 #if defined(__MINGW32__) || defined(_WIN32) || defined(WIN32)
  #define _IS_PLATFORM_WIN_
+ #include <windows.h>
 #endif
 
 
@@ -79,40 +80,6 @@
 
 
 
-
-
-#ifdef _IS_PLATFORM_WIN_
-
-/*
-   Implementation as per:
-   The Open Group Base Specifications, Issue 6
-   IEEE Std 1003.1, 2004 Edition
-   The timezone pointer arg is ignored.  Errors are ignored.
-*/
-
-#ifdef	__cplusplus
-
-void  GetSystemTimeAsFileTime(FILETIME*);
-
-inline int gettimeofday(struct timeval* p, void* tz /* IGNORED */)
-{
-	union {
-	    long long ns100; /*time since 1 Jan 1601 in 100ns units */
-		FILETIME ft;
-	} now;
-
-    GetSystemTimeAsFileTime( &(now.ft) );
-    p->tv_usec=(long)((now.ns100 / 10LL) % 1000000LL );
-    p->tv_sec= (long)((now.ns100-(116444736000000000LL))/10000000LL);
-	return 0;
-}
-
-#else
-    /* Must be defined somewhere else */
-	int gettimeofday(struct timeval* p, void* tz /* IGNORED */);
-#endif
-
-#endif
 
 
 #ifdef _IS_PLATFORM_WIN_
@@ -521,7 +488,6 @@ void dbg(int level, const char *fmt, ...)
 
     // fprintf(stderr, "free:002\n");
 }
-
 
 
 /*  return current UNIX time in microseconds (us). */
