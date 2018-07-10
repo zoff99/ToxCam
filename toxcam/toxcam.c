d2c408936f750ed01dedbc4a4bc905c6b408a905
zoff99 <zoff@zoff.cc>
2018-07-10 21:03:21 +0200

    added date/time to logoutput

--- toxcam/toxcam.c
+++ toxcam/toxcam.c
@@ -402,11 +402,11 @@ void dbg(int level, const char *fmt, ...)
         level = 0;
     }
 
-    level_and_format = malloc(strlen(fmt) + 3);
+    level_and_format = malloc(strlen(fmt) + 3 + 1);
 
     if (!level_and_format)
     {
-        // fprintf(stderr, "free:000a\n");
+        // dbg(9, stderr, "free:000a\n");
         return;
     }
 
@@ -431,22 +431,38 @@ void dbg(int level, const char *fmt, ...)
         level_and_format[0] = 'D';
     }
 
+    level_and_format[(strlen(fmt) + 2)] = '\0'; // '\0' or '\n'
+    level_and_format[(strlen(fmt) + 3)] = '\0';
+    time_t t3 = time(NULL);
+    struct tm tm3 = *localtime(&t3);
+    char *level_and_format_2 = malloc(strlen(level_and_format) + 5 + 3 + 3 + 1 + 3 + 3 + 3 + 1);
+    level_and_format_2[0] = '\0';
+    snprintf(level_and_format_2, (strlen(level_and_format) + 5 + 3 + 3 + 1 + 3 + 3 + 3 + 1),
+             "%04d-%02d-%02d %02d:%02d:%02d:%s",
+             tm3.tm_year + 1900, tm3.tm_mon + 1, tm3.tm_mday,
+             tm3.tm_hour, tm3.tm_min, tm3.tm_sec, level_and_format);
+
     if (level <= CURRENT_LOG_LEVEL)
     {
         va_list ap;
         va_start(ap, fmt);
-        vfprintf(logfile, level_and_format, ap);
+        vfprintf(logfile, level_and_format_2, ap);
         va_end(ap);
     }
 
-    // fprintf(stderr, "free:001\n");
+    // dbg(9, "free:001\n");
     if (level_and_format)
     {
-        // fprintf(stderr, "free:001.a\n");
+        // dbg(9, "free:001.a\n");
         free(level_and_format);
     }
 
-    // fprintf(stderr, "free:002\n");
+    if (level_and_format_2)
+    {
+        free(level_and_format_2);
+    }
+
+    // dbg(9, "free:002\n");
 }
 
 
